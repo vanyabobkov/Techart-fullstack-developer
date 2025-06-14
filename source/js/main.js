a3ac59faf5af93eae9d9f1b689ed5fb9da8a6baf
Ivan Bobkov <vanya.bobkov231195@gmail.com>
2025-06-14 21:34:12 +0300

    Add files via upload

--- source/js/main.js
+++ source/js/main.js
@@ -1,50 +1,7 @@
-//Превью выбранной статьи на банере
-const news = document.querySelectorAll('.news-thumbnails__item');
-const firstNews = news[0];
-const bannerNews = document.querySelector('.banner-news');
-firstNews.classList.add('news-thumbnails__item--current');
+import { changeDateFormat } from './date-format.js';
+import { addThumbnailClickHandler } from './preview-news.js';
+import { showCurrentPageElement } from './show-current-page.js';
 
-
-const imageOnBanner = firstNews.querySelector('.visually-hidden').textContent;
-bannerNews.style.backgroundImage = 'url(../../task/images/' + imageOnBanner + ')';
-
-const bannerNewsTitle = document.querySelector('.banner-news__title');
-bannerNewsTitle.textContent = firstNews.querySelector('.news-thumbnails__title').textContent;
-
-const bannerNewsText = document.querySelector('.banner-news__text');
-bannerNewsText.textContent = firstNews.querySelector('.news-thumbnails__announce').textContent;
-
-const Fn = (selectCurrentNews) => {
-  selectCurrentNews.addEventListener('click', function (evt) {
-    // evt.preventDefault();
-    const cancelCurrentNews = document.querySelector('.news-thumbnails__item--current');
-    cancelCurrentNews.classList.remove('news-thumbnails__item--current');
-    selectCurrentNews.classList.add('news-thumbnails__item--current');
-
-    imageToBanner = selectCurrentNews.querySelector('.visually-hidden').textContent;
-    bannerNewsTitle.textContent = selectCurrentNews.querySelector('.news-thumbnails__title').textContent;
-    bannerNewsText.textContent = selectCurrentNews.querySelector('.news-thumbnails__announce').textContent;
-    bannerNews.style.backgroundImage = 'url(../../task/images/' + imageToBanner + ')';
-  });
-}
-
-for (let i = 0; i < news.length; i++) {
-  Fn(
-    news[i],
-  );
-}
-
-//Выделение элемента пагинации текущей страницы
-const pageButtonsNode = document.querySelectorAll('.pagination__item');
-const pageButtons = [...pageButtonsNode];
-pageButtons[0].classList.add('pagination__item--current');
-const currentButton = document.querySelector('.pagination__item--current');
-let params = new URLSearchParams(document.location.search);
-let value = params.get('page');
-pageButtons.forEach(function (pageButton) {
-  const buttonValue = pageButton.querySelector('p').textContent;
-  if (value == buttonValue) {
-    currentButton.classList.remove('pagination__item--current');
-    pageButton.classList.add('pagination__item--current');
-  }
-});
+addThumbnailClickHandler();
+showCurrentPageElement();
+changeDateFormat();
