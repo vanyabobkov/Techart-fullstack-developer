6e261c454bdcca9c2c94ffee35a290182c351daf
Ivan Bobkov <vanya.bobkov231195@gmail.com>
2025-07-21 11:53:35 +0300

    Delete source directory

--- source/include/database.php
+++
@@ -1,9 +0,0 @@
-<?php
-$link = mysqli_connect("MySQL-8.0","root","","news");
-$result = mysqli_query($link, "SELECT * FROM `news`");
-
-if(mysqli_connect_errno())
-{
-    echo 'Ошибка подключения к БД ('.mysqli_connect_errno().'): '.mysqli_connect_error();
-    exit();
-}
--- source/js/main.js
+++
@@ -1,7 +0,0 @@
-import { changeDateFormat } from './date-format.js';
-import { addThumbnailClickHandler } from './preview-news.js';
-import { showCurrentPageElement } from './show-current-page.js';
-
-addThumbnailClickHandler();
-showCurrentPageElement();
-changeDateFormat();
